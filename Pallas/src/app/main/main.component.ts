1d01e95bd60b1b1000a5b8799fbbac53543afbf8
Eduardo Kamo <eduardokamoz@gmail.com>
2024-10-16 14:57:46 -0300

    Adicionado footer e home main

--- Pallas/src/app/main/main.component.ts
+++ Pallas/src/app/main/main.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
+import { HomeMainComponent } from '../home-main/home-main.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, HeaderComponent, FooterComponent, HomeMainComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
