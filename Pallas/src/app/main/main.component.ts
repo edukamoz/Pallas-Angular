931ee87c28c7158a73fbae3349282f2322f7d20d
Eduardo Kamo <gogeta1610@gmail.com>
2024-10-17 20:18:07 -0300

    Adicionado equipe

--- Pallas/src/app/main/main.component.ts
+++ Pallas/src/app/main/main.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { HomeMainComponent } from '../home-main/home-main.component';
+import { EquipeComponent } from '../equipe/equipe.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [FooterComponent, HeaderComponent, FooterComponent, HomeMainComponent],
+  imports: [FooterComponent, HeaderComponent, FooterComponent, HomeMainComponent, EquipeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
